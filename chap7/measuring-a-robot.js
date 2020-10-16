0e72eec0a8576d7a5a1196849ffaf9c6b6315f83
Ulrich <ulrichmabou@gmail.com>
2020-10-16 11:41:26 +0200

    Create measuring-a-robot.js

---
+++ chap7/measuring-a-robot.js
@@ -0,0 +1,40 @@
+/*
+It’s hard to objectively compare robots by just letting them solve a few scenarios.
+Maybe one robot just happened to get easier tasks or the kind of tasks that it is good at, 
+whereas the other didn’t.
+
+Write a function compareRobots that takes two robots (and their starting memory). 
+It should generate 100 tasks and let each of the robots solve each of these tasks.
+When done, it should output the average number of steps each robot took per task.
+
+For the sake of fairness, make sure you give each task to both robots, 
+rather than generating different tasks per robot.
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+  // Your code here
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
+*/
+
+function countSteps(state, robot, memory) {
+  for (let steps = 0;; steps++) {
+    if (state.parcels.length == 0) return steps;
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+  }
+}
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+  let total1 = 0, total2 = 0;
+  for (let i = 0; i < 100; i++) {
+    let state = VillageState.random();
+    total1 += countSteps(state, robot1, memory1);
+    total2 += countSteps(state, robot2, memory2);
+  }
+  console.log(`Robot 1 needed ${total1 / 100} steps per task`)
+  console.log(`Robot 2 needed ${total2 / 100}`)
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
